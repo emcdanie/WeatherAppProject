33ce7f94dfeb9ef8049767b25446e188f428d786
emcdanie <66241957+emcdanie@users.noreply.github.com>
2020-07-08 11:27:46 +0200

    created project space
    added html, css, js, src  to upload to githut

---
+++ src/script.js
@@ -0,0 +1,121 @@
+function formatDate() {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Nov",
+    "Dec",
+  ];
+
+  let currentMonth = month[date.getMonth()];
+  let currentYear = date.getFullYear();
+  let currentDay = days[date.getDay()];
+  let currentDate = date.getDate();
+
+  return `${currentDay} ${currentMonth} ${currentDate}, ${currentYear} | Time ${hours}:${minutes}`;
+}
+let currentTime = document.querySelector("#current-time");
+currentTime.innerHTML = formatDate();
+
+//current button search
+function getCurrentLocation() {
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let CurrentLocationButton = document.querySelector("#current-location-button");
+CurrentLocationButton.addEventListener("click", getCurrentLocation);
+
+//citysearch
+let searchButton = document.querySelector("#search-button");
+searchButton.addEventListener("click", handleSubmit);
+//city Search
+
+function searchCity(city, units) {
+  let apiKey = "717c122d03da5b502d476732c8793a31";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+  console.log("apiUrl");
+}
+
+function handleSubmit(event) {
+  let city = document.querySelector("#city-input").value;
+  event.preventDefault();
+  searchCity(city);
+}
+
+function searchLocation(position) {
+  let apiKey = "717c122d03da5b502d476732c8793a31";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+// data funcitons
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#status").innerHTML = response.data.weather[0].main;
+  document.querySelector("#sunrise").innerHTML = UTCtoTwentyFourHours(
+    response.data.timezone,
+    response.data.sys.sunrise
+  );
+
+  document.querySelector("#sunset").innerHTML = UTCtoTwentyFourHours(
+    response.data.timezone,
+    response.data.sys.sunset
+  );
+}
+
+//links
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", searchCity(city, "imperial"));
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", searchCity(city, "metric"));
+
+//sunset and sunrise hours
+
+function UTCtoTwentyFourHours(timeZone, time) {
+  let newTime = (time + timeZone) * 1000;
+  let convertedDate = new Date(newTime);
+  let convertedHrs = convertedDate.getUTCHours().toString().padStart(2, "0");
+  let convertedMins = convertedDate.getUTCMinutes().toString().padStart(2, "0");
+  let returnedString = convertedHrs + ":" + convertedMins;
+  return returnedString;
+}
+
+searchCity("Paris,France");

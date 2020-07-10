422026dedc667be2c1375e69559887c615cec094
emcdanie <66241957+emcdanie@users.noreply.github.com>
2020-07-10 20:11:23 +0200

    styling amendments

--- src/script.js
+++ src/script.js
@@ -96,7 +96,7 @@ function displayForecast(response) {
   src = "http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
 />
   <div class="weather-forecast-temperature">
-  <strong>${Math.round(forecast.main.temp_max)}°</strong>${Math.round(
+  <strong>${Math.round(forecast.main.temp_max)}° |  </strong>${Math.round(
       forecast.main.temp_min
     )}°
 </div>
